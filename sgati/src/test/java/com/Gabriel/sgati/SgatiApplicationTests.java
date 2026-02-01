23927217a0b0d9932c9f1c118f79afb3a6118476
Gabriel Silva <gaabdev42@gmail.com>
2026-02-01 13:17:04 -0400

    feat: initial Spring Boot project

---
+++ sgati/src/test/java/com/Gabriel/sgati/SgatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gabriel.sgati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
